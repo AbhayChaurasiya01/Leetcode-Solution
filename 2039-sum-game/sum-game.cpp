dd6eb81ce5b9ae72f62b94816a7bec90aa43deb7
Abhay Chaurasiya <iamabhaychaurasiya@gmail.com>
2026-08-23 09:51:25 +0530

    Time: 4 ms (47.55%) | Memory: 14 MB (33.57%) - LeetSync

---
+++ 2039-sum-game/sum-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?')
+                leftQ++;
+            else
+                leftSum += num[i] - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?')
+                rightQ++;
+            else
+                rightSum += num[i] - '0';
+        }
+
+        // Odd number of '?' means Alice wins
+        if ((leftQ + rightQ) % 2 == 1)
+            return true;
+
+        // Bob wins only when the sums can be balanced
+        return leftSum - rightSum != 9 * (rightQ - leftQ) / 2;
+    }
+};
